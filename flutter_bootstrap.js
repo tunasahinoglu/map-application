946cdd7a16b0e271cf8e462132ce07ee3ed960f7
tuna <tuna.sahinoglu@hotmail.com>
2025-01-14 13:55:27 +0300

    web final

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "840322796"
+    serviceWorkerVersion: "1953313753"
   }
 });
